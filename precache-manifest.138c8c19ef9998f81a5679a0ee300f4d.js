6a0bef04358785a006a0ba4a93a55f5c34fbe603
Steffen Hytrek <steffen.hytrek@gmail.com>
2020-02-08 17:33:43 +0100

    Updates

---
+++ precache-manifest.138c8c19ef9998f81a5679a0ee300f4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecc2fb6d6d500e20234ca042e6b3e33",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "68389b7aaf429702edff",
+    "url": "/monsters-rolodex/static/css/main.e28b3a17.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68389b7aaf429702edff",
+    "url": "/monsters-rolodex/static/js/main.7e995979.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
